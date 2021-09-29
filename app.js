1151a3446ad3252ad4d16bdcee58f0665df54e18
Ludmila-Eybog <ludm.nvkz@gmail.com>
2021-09-29 12:16:42 +0700

    add 4

---
+++ app.js
@@ -0,0 +1,18 @@
+if (true) {
+  alert('true')
+}
+
+// class Car {
+//   constructor() {
+//     super()
+//   }
+// }
+
+// get() {
+//   setTimeout(() => {
+//     const obj = {
+//       a: 1,
+//     }
+//   })
+// }
+
